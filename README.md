10d62d1c75d07bf905e2f459b22212cddbf08c82
Tomas Barton <barton.tomas@gmail.com>
2014-04-10 00:48:21 +0200

    refactored module

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Puppet module: sudoers
 
-This is the sudoers module.
-
-This is based on https://github.com/windowsrefund/puppet-sudoers
+Module for mamaging sudoers file.
 
 ## Usage
 
 `manifests/nodes.pp`:
 
-    class {'sudoers': }
-
+```puppet
+class {'sudoers': }
+```
 
 Hiera file e.g. `Debian.yaml`
- 
+
+```yaml
     sudoers_package: 'sudo'
     sudoers_config_file: '/etc/sudoers'
     sudoers_rules:
@@ -23,25 +23,8 @@ Hiera file e.g. `Debian.yaml`
       '%sudo':
         ensure: present # or absent
         commands: ALL
-
+```
 
 ## License
 
 [GNU-GPLv3](https://github.com/deric/puppet-sudoers/blob/master/GNU-GPL.txt)
-
-## Contact
-
-
-Super-Visions
-
-Pathoekeweg 120 b bus 1.07
-8000 Brugge
-tel: +32 50 68 55 58
-mail: info@super-visions.com
-
-http://super-visions.com
-
-## Support
-
-
-Please log tickets and issues at https://github.com/Super-Visions/puppet-sudoers
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,62 @@
 #
 # == Class: sudoers
 #
-# The sudoers class manges the /etc/sudoers file based on data stored in hiera
-#
-# === Parameters
-#
-# This class does not take parameters as all parameters are fetched via hiera
-# The parameter defaults are stored in the sudoers::data class
-#
-# === Variables
-#
-# No variables are used in the sudoers base class
+# The sudoers class manges the /etc/sudoers file
 #
 # === Examples
 #
 #  class { sudoers: }
 #
+# === Hiera
+#sudoers::rules:
+#  someuser:
+#    ensure: present
+#    nopasswd: 'true'
+#    commands: ls
+#  '%agroup':
+#    ensure: absent
+#    nopasswd: 'false'
+#    commands: ALL
+#
 # === Authors
 #
-# Stefan Goethals <stefan.goethals@super-visions.com>
-#
-# === Copyright
-#
-# Copyright 2012 Super-Visions, unless otherwise noted.
-#
-class sudoers
+class sudoers(
+  $package = undef,
+  $ensure  = 'present',
+  $rules   = {}
+)
 {
-  anchor { 'sudoers::start': }->
-  class { 'sudoers::package': }->
-  class { 'sudoers::config': }->
+  validate_hash($rules)
+
+  anchor { 'sudoers::start': }
+
+  if $package == undef {
+    case $::operatingsystem {
+      ubuntu, debian: {
+        $package     = 'sudo'
+        $config_file = '/etc/sudoers'
+      }
+      centos, redhat: {
+        $package     = 'sudo'
+        $config_file = '/etc/sudoers'
+      }
+      default: {
+        fail("Unknown OS: $::operatingsystem, you have to specify $package param")
+      }
+    }
+  }
+
+  package { $package:
+    ensure  => $ensure,
+    require => Anchor['sudoers::start']
+  }
+
+  if($rules) {
+    create_resources(sudoers, $rules, {
+      'require' => Package[$package],
+      'before'  => Anchor['sudoers::end']
+    })
+  }
+
   anchor { 'sudoers::end': }
 }
